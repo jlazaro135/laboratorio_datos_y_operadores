c316b461d11c74a9e2f2a5b372f33d6320b8cd8e
jlazaro <jlazaro135@gmail.com>
2023-05-14 13:36:51 +0200

    se añaden screenshots y se completa el README

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Muestra por consola del resultado del ejercicio propuesto en el laboratorio
 
+Para dar un poco más de verbosidad al ejercicio he usado "template literals" para interpolar texto con las operaciones
+
+[consola](public/screenshots/consola.png)
+
+[template literals](public/screenshots/main.ts.png)
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-console.log(`Como estoy un poco tieso de pasta, solo puedo invitar a la bebida, en vez de ${(120)/6}€, vosotros pagáis ${(120-18)/6}€ y yo pago ${(120-18)/6 + 18}€`)
+console.log(
+  `Como estoy un poco tieso de pasta, solo puedo invitar a la bebida, en vez de ${
+    120 / 6
+  }€, vosotros pagáis ${(120 - 18) / 6}€ y yo pago ${(120 - 18) / 6 + 18}€`
+);
