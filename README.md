c14a4086cc502cb08782fb8e63e5190882a4ecc4
jlazaro <jlazaro135@gmail.com>
2023-05-14 13:19:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MODULO 2 - TIPO DE DATOS Y OPERADORES
+
+Muestra por consola del resultado del ejercicio propuesto en el laboratorio
+
---
+++ src/main.ts
@@ -0,0 +1,1 @@
+console.log(`Como estoy un poco tieso de pasta, solo puedo invitar a la bebida, en vez de ${(120)/6}€, vosotros pagáis ${(120-18)/6}€ y yo pago ${(120-18)/6 + 18}€`)
